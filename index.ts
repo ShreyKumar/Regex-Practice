0148538ec89d7a611a683627ca0235bc65d967ab
Shreyansh Kumar <shreyansh.k@myplanet.com>
2021-01-08 19:00:34 -0500

    excercise 3

--- index.ts
+++ index.ts
@@ -32,33 +32,69 @@
 
 // console.log(re.test(grey), re.test(gray), re.test(griy))
 
-const charSet = /[rh][abcde][ i]/g
-console.log(charSet.test('Make the outline for the square gray and fill the circle grey.'))
+// const charSet = /[rh][abcde][ i]/g
+// console.log(charSet.test('Make the outline for the square gray and fill the circle grey.'))
 
-const regex6 = /[1-4][1-6]/
-const txt4 = "`1234"
-const regex7 = /[1\-4]/
-const txt5 = "1-4"
-console.log(txt5.match(regex7))
+// const regex6 = /[1-4][1-6]/
+// const txt4 = "`1234"
+// const regex7 = /[1\-4]/
+// const txt5 = "1-4"
+// console.log(txt5.match(regex7))
 
-const regex8 = /[10-20]/g
-const txt6 = "9"
+// const regex8 = /[10-20]/g
+// const txt6 = "9"
 
-console.log(txt6.match(regex8))
+// console.log(txt6.match(regex8))
 
-const regex9 = /[\^a-z]/
-const txt7 = "abcdef^"
-console.log(regex9.exec(txt7))
+// const regex9 = /[\^a-z]/
+// const txt7 = "abcdef^"
+// console.log(regex9.exec(txt7))
 
-// \s matches \r \t \n and ' '
-const regex10 = /\w\w\w/
+// // \s matches \r \t \n and ' '
+// const regex10 = /\w\w\w/
 
-const newPhoneNums = ["801-766-9754", "801-545-5454", "435-666-1212", "801-796-8010", "435-555-9801", "801-009-0909", "435-222-8013", "801-777-66553", "801-777-665-", "801-77A-6655", "801-778-665"]
+// const newPhoneNums = ["801-766-9754", "801-545-5454", "435-666-1212", "801-796-8010", "435-555-9801", "801-009-0909", "435-222-8013", "801-777-66553", "801-777-665-", "801-77A-6655", "801-778-665"]
 
-const validPhones = (phoneNums) => {
-  const phonePattern = /801-[0-9]{3}-[0-9]{4}/
-  return phoneNums.filter((num) => phonePattern.test(num))
-}
+// const validPhones = (phoneNums) => {
+//   const phonePattern = /801-[0-9]{3}-[0-9]{4}/
+//   return phoneNums.filter((num) => phonePattern.test(num))
+// }
 
 
-console.log(validPhones(newPhoneNums))
+// console.log(validPhones(newPhoneNums))
+
+// const regex11 = /[A-Z]*/
+// const str = 'She sells Seashells on a seashore'
+
+// const regex12 = /s[a-z]?/
+
+// console.log(str.match(regex11))
+// console.log(str.match(regex12))
+
+
+// const regex13 = /warning!*/
+// const warning = 'warninffg!'
+// console.log(warning.match(regex13))
+
+// const someHTML = '<p>This is the first paragraph.</p><p>This is the first paragraph.</p>'
+// const regex14 = /<p>.*?<\/p>/
+// console.log(someHTML.match(regex14))
+
+// const regex15 = /\w{3,5}/g
+// const num = 'My telephone number is as follows: 801-555-6789'
+
+// const regex16 = /\d{3}-\d{2}-\d{3}/g
+
+// const validPhone = /\(?[0-9]{3}\)?[-.]?[0-9]{3}[-.]?[0-9]{4}/
+
+// const phones = [
+//   '(123)-123-1234',
+//   '123.123.1234',
+//   '123-123-1234',
+//   '1231231234',
+//   '(123)123-1234',
+//   '123--123-1234' // should fail
+// ]
+
+// console.log(phones.map((p) => p.match(validPhone)))
+
